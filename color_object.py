4f7c027de5208b77e3dfd959a15171daa691c44d
Shiven Saini <shivensaini73@gmail.com>
2024-09-03 19:19:11 +0530

    changing color space

---
+++ color_object.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+if not cap.isOpened() :
+    print("Unable to launch camera!")
+    exit()
+
+while 1:
+    _, frame_BGR  = cap.read()
+
+    frame_HSV = cv.cvtColor(frame_BGR, cv.COLOR_BGR2HSV)
+
+    # Defining HSV Bounds to gather.
+    lower_bound = np.array([170, 150, 50])
+    upper_bound = np.array([180, 255, 255])
+
+    # frame data only in the given bound
+    mask = cv.inRange(frame_HSV, lower_bound, upper_bound)
+
+    result = cv.bitwise_and(frame_BGR, frame_BGR, mask=mask)
+
+    cv.imshow("Video raw", frame_BGR)
+    cv.imshow("Mask", mask)
+    cv.imshow("Result", result)
+
+    if cv.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
+
