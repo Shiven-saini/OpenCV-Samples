c49e770a15810e5faca0e1a4b51af2f5363f8418
Shiven Saini <shivensaini73@gmail.com>
2024-09-05 19:16:53 +0530

    added simple threshold

--- threshold_simple.py
+++ threshold_simple.py
@@ -4,7 +4,9 @@ import numpy as np
 img = cv.imread("samples/gradient.png", cv.IMREAD_GRAYSCALE)
 assert img is not None, "Sorry, unable to find the image!"
 
-ret, thresh1 = cv.threshold(img, 127, 180, cv.THRESH_BINARY)
+ret, thresh1 = cv.threshold(img, 80, 180, cv.THRESH_BINARY)
+print(ret)
+
 ret, thresh2 = cv.threshold(img, 127, 255, cv.THRESH_TRUNC)
 ret, thresh3 = cv.threshold(img, 127, 255, cv.THRESH_TOZERO)
 
