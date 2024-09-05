9c6e763d671ecf79cb255ca083bd64612c035669
Shiven Saini <shivensaini73@gmail.com>
2024-09-06 00:37:55 +0530

    added thresholding sources and otsu binarization algo

--- threshold_otsu.py
+++ threshold_otsu.py
@@ -1,5 +1,21 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread("samples/noisy.jpg", cv.IMREAD_GRAYSCALE)
+img = cv.imread("samples/otsu-raw.jpeg", cv.IMREAD_GRAYSCALE)
+
+
+# global simple thresholding
+ret, th1 = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+
+#Otsu's thresholding
+ret, th2 = cv.threshold(img, 0, 25, cv.THRESH_BINARY+cv.THRESH_OTSU)
+
+
+cv.imshow("Original Image", img)
+cv.imshow("Simple threshold", th1)
+cv.imshow("Otsu's processed", th2)
+
+
+cv.waitKey(0)
+cv.destroyAllWindows()
 
