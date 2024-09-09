72bd91c936fea570813e2a23b64aabeeea89b1ad
Shiven Saini <shivensaini73@gmail.com>
2024-09-09 22:05:56 +0530

    construct canvas both black and white

--- create_canvas.py
+++ create_canvas.py
@@ -3,9 +3,13 @@ import numpy as np
 
 source = cv.imread("samples/messi5.jpg")
 
-# black canvas of the same dimension as source.
-canvas = np.zeros_like(source)
-cv.imshow("Canvas", canvas)
+# Black and white canvas of the same dimension as source.
+canvas = np.ones_like(source)
+canvas_white = canvas*255
+canvas_black = canvas*0
+
+cv.imshow("Canvas", canvas_white)
+cv.imshow("Canvas black", canvas_black)
 
 if cv.waitKey(0) == ord('q') & 0xFF:
     cv.destroyAllWindows()
